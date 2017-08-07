479dfe2e43509730d9b8b0c6bfe4d3d566c9e960
Masayuki Morita <masayuki.morita@crowdworks.co.jp>
2017-08-07 18:20:28 +0900

    Change package name to cyqldog

--- cyqldog/dogstatd.go
+++ cyqldog/dogstatd.go
@@ -1,4 +1,4 @@
-package monitor
+package cyqldog
 
 import (
 	"fmt"
--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 	"os/signal"
 	"syscall"
 
-	"github.com/crowdworks/cyqldog/monitor"
+	"github.com/crowdworks/cyqldog/cyqldog"
 	_ "github.com/lib/pq"
 )
 
@@ -26,37 +26,37 @@ func main() {
 func run(configPath string) {
 	// Load the configuration file.
 	log.Printf("main: load config file: %s", configPath)
-	config, err := monitor.NewConfig(configPath)
+	config, err := cyqldog.NewConfig(configPath)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	// Connect to the database.
-	db, err := monitor.NewDB(config.Db)
+	db, err := cyqldog.NewDB(config.Db)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
 
 	// Connect to the dogstatsd.
-	statsd, err := monitor.NewStatsd(config.Statd)
+	statsd, err := cyqldog.NewStatsd(config.Statd)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	// Make a task queue for monitoring job.
-	q := make(chan monitor.Rule)
+	q := make(chan cyqldog.Rule)
 
 	// Make a scheduler for each rule.
 	for i, rule := range config.Rules {
-		scheduler := monitor.NewScheduler(i, rule)
+		scheduler := cyqldog.NewScheduler(i, rule)
 		go scheduler.Run(q)
 	}
 
 	// Make a monitoring worker.
 	// In order to limit the number of DB connection to 1 for monitoring,
 	// only one worker should run.
-	m := monitor.NewMonitor(db, statsd)
+	m := cyqldog.NewMonitor(db, statsd)
 	go m.Run(q)
 
 	// Trap signals from OS for normal termination.
