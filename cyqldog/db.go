6a9ce6b2f309d4ff8ea1cdcb5d74947acc0146bf
Masayuki Morita <masayuki.morita@crowdworks.co.jp>
2017-08-12 11:44:02 +0900

    Change the data source configuration format to support other driver.

--- cyqldog/db.go
+++ cyqldog/db.go
@@ -2,7 +2,7 @@ package cyqldog
 
 import (
 	"database/sql"
-	"fmt"
+	"strings"
 
 	"github.com/pkg/errors"
 )
@@ -12,47 +12,45 @@ type DataSource struct {
 	// Driver is type of the database to connect.
 	// Currently suppoted database is postgres.
 	Driver string `yaml:"driver"`
-	// Host is hostname or IP address of the database.
-	Host string `yaml:"host"`
-	// Port is a port number of the database.
-	Port string `yaml:"port"`
-	// User is a username of the database.
-	User string `yaml:"user"`
-	// Password is a password of the database.
-	Password string `yaml:"password"`
-	// Dbname is a name of the database.
-	Dbname string `yaml:"dbname"`
-	// Sslmode is an SSL connection option.
-	// Suppoted values are `require`, `verify-ca`, `verify-full`, and `disable`.
-	Sslmode string `yaml:"sslmode"`
+	// Options is a map of options to connect.
+	// These options are passed to sql.Open.
+	// The supported options are depend on the database driver.
+	Options DataSourceOptions `yaml:"options"`
 }
+
+// DataSourceOptions is a map of options to connect.
+type DataSourceOptions map[string]string
 
 // newDB returns an instance of sql.DB.
 // This function returns a error if the connection test fails.
 func newDB(s DataSource) (*sql.DB, error) {
-	dataSourceName := fmt.Sprintf(
-		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
-		s.Host, s.Port, s.User, s.Password, s.Dbname, s.Sslmode,
-	)
+
+	// Join the options into a string of data source name.
+	dataSourceName := s.getDataSourceName()
 
 	// Open the database.
 	// Note that network connection is not established at this time.
 	db, err := sql.Open(s.Driver, dataSourceName)
 	if err != nil {
-		return nil, errors.Wrapf(err,
-			"failed to open database: driver=%s host=%s port=%s user=%s dbname=%s sslmode=%s",
-			s.Driver, s.Host, s.Port, s.User, s.Dbname, s.Sslmode,
-		)
+		return nil, errors.Wrapf(err, "failed to open database")
 	}
 
 	// Connect to the database and verify its connection.
 	if err = db.Ping(); err != nil {
 		db.Close()
 
-		return nil, errors.Wrapf(err,
-			"failed to connect database: driver=%s host=%s port=%s user=%s dbname=%s sslmode=%s",
-			s.Driver, s.Host, s.Port, s.User, s.Dbname, s.Sslmode,
-		)
+		return nil, errors.Wrapf(err, "failed to connect database")
 	}
 	return db, nil
+}
+
+// getDataSourceName returns a data source name to use for sql.Open.
+func (s *DataSource) getDataSourceName() string {
+	opts := make([]string, len(s.Options))
+	for k, v := range s.Options {
+		o := k + "=" + v
+		opts = append(opts, o)
+	}
+
+	return strings.Join(opts[:], " ")
 }
