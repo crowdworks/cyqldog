f8f6e000fd2a25ddb2742ded087efbdba9d21fe9
Masayuki Morita <masayuki.morita@crowdworks.co.jp>
2017-08-22 14:12:26 +0900

    Rename DataSource to DataSourceConfig

--- cyqldog/data_source.go
+++ cyqldog/data_source.go
@@ -8,8 +8,8 @@ import (
 	"github.com/pkg/errors"
 )
 
-// DataSource is a configuration of the database to connect.
-type DataSource struct {
+// DataSourceConfig is a configuration of the database to connect.
+type DataSourceConfig struct {
 	// Driver is type of the database to connect.
 	// Currently suppoted databases are as follows:
 	//  - postgres
@@ -26,7 +26,7 @@ type DataSourceOptions map[string]string
 
 // newDB returns an instance of sql.DB.
 // This function returns a error if the connection test fails.
-func newDB(s DataSource) (*sql.DB, error) {
+func newDB(s DataSourceConfig) (*sql.DB, error) {
 
 	// Join the options into a string of data source name.
 	dataSourceName, err := s.getDataSourceName()
@@ -51,7 +51,7 @@ func newDB(s DataSource) (*sql.DB, error) {
 }
 
 // getDataSourceName returns a data source name to use for sql.Open.
-func (s *DataSource) getDataSourceName() (string, error) {
+func (s *DataSourceConfig) getDataSourceName() (string, error) {
 	// Check database driver
 	switch s.Driver {
 	case "postgres":
@@ -63,7 +63,7 @@ func (s *DataSource) getDataSourceName() (string, error) {
 	}
 }
 
-func (s *DataSource) getDataSourceNamePostgres() (string, error) {
+func (s *DataSourceConfig) getDataSourceNamePostgres() (string, error) {
 	opts := make([]string, len(s.Options))
 	for k, v := range s.Options {
 		o := k + "=" + v
@@ -73,7 +73,7 @@ func (s *DataSource) getDataSourceNamePostgres() (string, error) {
 	return strings.Join(opts[:], " "), nil
 }
 
-func (s *DataSource) getDataSourceNameMySQL() (string, error) {
+func (s *DataSourceConfig) getDataSourceNameMySQL() (string, error) {
 	o := s.Options
 
 	// render DSN format
